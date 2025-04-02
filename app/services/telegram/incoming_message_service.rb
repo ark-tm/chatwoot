7134e137e326a5c598f5b3a0c5d9b79b9cf0dcbf
platsajacki <menyukhov@bk.ru>
2025-04-02 08:42:56 +0300

    feat: Enhance logging in IncomingMessageService for better traceability

--- app/services/telegram/incoming_message_service.rb
+++ app/services/telegram/incoming_message_service.rb
@@ -1,18 +1,20 @@
-# Find the various telegram payload samples here: https://core.telegram.org/bots/webhooks#testing-your-bot-with-updates
-# https://core.telegram.org/bots/api#available-types
-
 class Telegram::IncomingMessageService
   include ::FileTypeHelper
   include ::Telegram::ParamHelpers
   pattr_initialize [:inbox!, :params!]
 
   def perform
-    # chatwoot doesn't support group conversations at the moment
-    return unless private_message?
+    Rails.logger.info('[IncomingMessageService] Выполнение perform начато')
+    unless private_message?
+      Rails.logger.info('[IncomingMessageService] Сообщение не приватное, завершаем выполнение')
+      return
+    end
 
     set_contact
     update_contact_avatar
     set_conversation
+
+    Rails.logger.info('[IncomingMessageService] Создаем новое сообщение')
     @message = @conversation.messages.build(
       content: telegram_params_message_content,
       account_id: @inbox.account_id,
@@ -25,11 +27,13 @@ class Telegram::IncomingMessageService
 
     process_message_attachments if message_params?
     @message.save!
+    Rails.logger.info('[IncomingMessageService] Сообщение сохранено успешно')
   end
 
   private
 
   def set_contact
+    Rails.logger.info('[IncomingMessageService] Начато выполнение set_contact')
     contact_inbox = ::ContactInboxWithContactBuilder.new(
       source_id: telegram_params_from_id,
       inbox: inbox,
@@ -38,22 +42,35 @@ class Telegram::IncomingMessageService
 
     @contact_inbox = contact_inbox
     @contact = contact_inbox.contact
+    Rails.logger.info("[IncomingMessageService] Контакт установлен: #{@contact.id}")
   end
 
   def process_message_attachments
+    Rails.logger.info('[IncomingMessageService] Начато выполнение process_message_attachments')
     attach_location
     attach_files
     attach_contact
+    Rails.logger.info('[IncomingMessageService] Завершено выполнение process_message_attachments')
   end
 
   def update_contact_avatar
-    return if @contact.avatar.attached?
+    Rails.logger.info('[IncomingMessageService] Начато выполнение update_contact_avatar')
+    if @contact.avatar.attached?
+      Rails.logger.info('[IncomingMessageService] Аватар уже прикреплён, пропускаем')
+      return
+    end
 
     avatar_url = inbox.channel.get_telegram_profile_image(telegram_params_from_id)
-    ::Avatar::AvatarFromUrlJob.perform_later(@contact, avatar_url) if avatar_url
+    if avatar_url
+      Rails.logger.info("[IncomingMessageService] Найден URL аватара: #{avatar_url}")
+      ::Avatar::AvatarFromUrlJob.perform_later(@contact, avatar_url)
+    else
+      Rails.logger.info('[IncomingMessageService] URL аватара не найден')
+    end
   end
 
   def conversation_params
+    Rails.logger.info('[IncomingMessageService] Формирование параметров беседы')
     {
       account_id: @inbox.account_id,
       inbox_id: @inbox.id,
@@ -64,13 +81,19 @@ class Telegram::IncomingMessageService
   end
 
   def set_conversation
+    Rails.logger.info('[IncomingMessageService] Начато выполнение set_conversation')
     @conversation = @contact_inbox.conversations.first
-    return if @conversation
+    if @conversation
+      Rails.logger.info("[IncomingMessageService] Беседа найдена: #{@conversation.id}")
+      return
+    end
 
     @conversation = ::Conversation.create!(conversation_params)
+    Rails.logger.info("[IncomingMessageService] Создана новая беседа: #{@conversation.id}")
   end
 
   def contact_attributes
+    Rails.logger.info('[IncomingMessageService] Формирование параметров контакта')
     {
       name: "#{telegram_params_first_name} #{telegram_params_last_name}",
       additional_attributes: additional_attributes
@@ -79,7 +102,6 @@ class Telegram::IncomingMessageService
 
   def additional_attributes
     {
-      # TODO: Remove this once we show the social_telegram_user_name in the UI instead of the username
       username: telegram_params_username,
       language_code: telegram_params_language_code,
       social_telegram_user_id: telegram_params_from_id,
@@ -94,6 +116,7 @@ class Telegram::IncomingMessageService
   end
 
   def file_content_type
+    Rails.logger.info('[IncomingMessageService] Определение типа файла')
     return :image if params[:message][:photo].present? || params.dig(:message, :sticker, :thumb).present?
     return :audio if params[:message][:voice].present? || params[:message][:audio].present?
     return :video if params[:message][:video].present?
@@ -102,17 +125,17 @@ class Telegram::IncomingMessageService
   end
 
   def attach_files
+    Rails.logger.info('[IncomingMessageService] Начато выполнение attach_files')
     return unless file
 
     file_download_path = inbox.channel.get_telegram_file_path(file[:file_id])
     if file_download_path.blank?
-      Rails.logger.info "Telegram file download path is blank for #{file[:file_id]} : inbox_id: #{inbox.id}"
+      Rails.logger.info "[IncomingMessageService] Путь загрузки файла пуст для #{file[:file_id]} : inbox_id: #{inbox.id}"
       return
     end
 
-    attachment_file = Down.download(
-      inbox.channel.get_telegram_file_path(file[:file_id])
-    )
+    attachment_file = Down.download(file_download_path)
+    Rails.logger.info("[IncomingMessageService] Файл скачан: #{attachment_file.original_filename}")
 
     @message.attachments.new(
       account_id: @message.account_id,
@@ -123,9 +146,11 @@ class Telegram::IncomingMessageService
         content_type: attachment_file.content_type
       }
     )
+    Rails.logger.info('[IncomingMessageService] Файл прикреплен к сообщению')
   end
 
   def attach_location
+    Rails.logger.info('[IncomingMessageService] Начато выполнение attach_location')
     return unless location
 
     @message.attachments.new(
@@ -135,9 +160,11 @@ class Telegram::IncomingMessageService
       coordinates_lat: location['latitude'],
       coordinates_long: location['longitude']
     )
+    Rails.logger.info('[IncomingMessageService] Локация прикреплена к сообщению')
   end
 
   def attach_contact
+    Rails.logger.info('[IncomingMessageService] Начато выполнение attach_contact')
     return unless contact_card
 
     @message.attachments.new(
@@ -149,13 +176,16 @@ class Telegram::IncomingMessageService
         last_name: contact_card['last_name']
       }
     )
+    Rails.logger.info('[IncomingMessageService] Контакт прикреплен к сообщению')
   end
 
   def file
+    Rails.logger.info('[IncomingMessageService] Определение файла')
     @file ||= visual_media_params || params[:message][:voice].presence || params[:message][:audio].presence || params[:message][:document].presence
   end
 
   def location_fallback_title
+    Rails.logger.info('[IncomingMessageService] Определение fallback_title для location')
     return '' if venue.blank?
 
     venue[:title] || ''
@@ -166,14 +196,17 @@ class Telegram::IncomingMessageService
   end
 
   def location
+    Rails.logger.info('[IncomingMessageService] Определение location')
     @location ||= params.dig(:message, :location).presence
   end
 
   def contact_card
+    Rails.logger.info("[IncomingMessageService] Определение contact_card. Полные параметры: #{params.inspect}")
     @contact_card ||= params.dig(:message, :contact).presence
   end
 
   def visual_media_params
+    Rails.logger.info('[IncomingMessageService] Определение визуальных медиа параметров')
     params[:message][:photo].presence&.last || params.dig(:message, :sticker, :thumb).presence || params[:message][:video].presence
   end
 end
